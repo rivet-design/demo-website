3d8da37f9e5176af2973f68fd696ac3a84470571
samjgorman <samjgorman@gmail.com>
2026-08-29 23:00:33 -0700

    Keep about title on one line

--- src/blog/AboutPage.tsx
+++ src/blog/AboutPage.tsx
@@ -26,7 +26,7 @@ const AboutPage = () => {
     <BlogShell>
       <article className="mx-auto w-full max-w-6xl px-4 pb-28 pt-16 sm:px-8 sm:pt-24">
         <header className="mx-auto max-w-[42rem] text-left">
-          <h1 className="font-main text-4xl font-normal leading-[1.08] tracking-[-0.035em] text-black sm:text-5xl xl:whitespace-nowrap">
+          <h1 className="font-main text-4xl font-normal leading-[1.08] tracking-[-0.035em] text-black md:whitespace-nowrap">
             {meta.title}
           </h1>
         </header>
